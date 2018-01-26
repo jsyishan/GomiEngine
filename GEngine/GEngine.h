5334e1ff3fcc89f014ea62d7e7508cb330268fe9
jsyishan <404678173@qq.com>
2018-01-26 22:14:30 +0800

    revise

---
+++ GEngine/GEngine.h
@@ -0,0 +1,10 @@
+#ifndef GENGINE_H
+#define GENGINE_H
+
+#include "GEngine/math/GEConstant.h"
+#include "GEngine/math/GEHelper.h"
+#include "GEngine/math/GEMatrix33.h"
+#include "GEngine/math/GEQuaternion.h"
+#include "GEngine/math/GEVector.h"
+
+#endif
